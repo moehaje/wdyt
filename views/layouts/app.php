9b2772d6941ed095e65a8e5c39bc70c4970f82e5
Moe Haje <91034104+moehaje@users.noreply.github.com>
2024-06-27 14:57:56 +0200

    update-views-w/-new-styling-system

--- views/layouts/app.php
+++ views/layouts/app.php
@@ -1,20 +1,17 @@
 <!DOCTYPE html>
-<html lang="en" class="h-full bg-gray-100">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?? 'WDY' ?></title>
-    <link href="/css/output.css" rel="stylesheet">
+    <link href="/css/purged.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <script type="module" src="/js/index.js" defer></script>
 </head>
-<body class="h-full">
-    <div class="min-h-full">
-        <?php require base_path('views/partials/nav.php') ?>
-
-        <main>
-            <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                <?= $content ?>
-            </div>
-        </main>
-    </div>
+<body>
+    <?php require base_path('views/partials/nav.php') ?>
+    <?= $content ?>
 </body>
 </html>
